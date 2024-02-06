ec95bb09345f6f6270ad9513d8a7293aa41e935e
Federico Panasci <feedeeblk@gmail.com>
2024-02-06 17:55:36 -0300

    segunda demo jugable

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ function App() {
   const [isClicked, setIsClicked] = useState(false);
   const [buttonInciar, setButtonIniciar] = useState(false);
   const [buttonColorDisable, setButtonColorDisable] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
 
   const colors = ['red', 'green', 'yellow', 'blue']
+
+  const playSound = (color) => {
+    const audio = new Audio(`/sound/audio-${color}.mp3`);
+    audio.play();
+  };
 
   const handleClick = ((color) => {
     setButtonColor(color);
     setIsClicked(true);
+    playSound(color);
     setPlayerColor([...playerColor, color]);
     setTimeout(() => {
       setIsClicked(false);
@@ -33,22 +40,20 @@ function App() {
   }
 
   useEffect(() => {
-    if (buttonInciar) {
-      addComColor();
-    }
-  }, [buttonInciar])
-
-  useEffect(() => {
     if (comColor.length > 0) {
       showSequence();
     }
   }, [comColor])
 
-  const showSequence = () => {
+  const showSequence = useCallback(() => {
     let index = 0;
     const interval = setInterval(() => {
         setButtonColor(comColor[index]);
         setIsClicked(true);
+        if (comColor[index] !== undefined) {
+          const audio = new Audio(`/sound/audio-${comColor[index]}.mp3`);
+          audio.play();
+        }
         setTimeout(() => {
         setIsClicked(false);
         setButtonColor('');
@@ -61,17 +66,25 @@ function App() {
         }
       }, 300);
     }, 1000);
-  }
+  })
 
   const comprobarGame = useCallback(() => {
     if (playerColor[playerColor.length - 1] !== comColor[playerColor.length - 1]) {
-      alert("Las secuencias son diferentes. ¡PERDISTE! Haz click en Iniciar para volver a jugar.");
+      const audio = new Audio(`/sound/error.wav`);
+      audio.play();
+      setButtonIniciar(false);
+      setButtonColorDisable(true);
       setComColor([])
-      setPlayerColor([])
       return;
   } else {
     if (playerColor.length === comColor.length) {
-      alert("Las secuencias son iguales. ¡Haz click en iniciar para el siguiente nivel!");
+      setTimeout(() => {
+        const audio = new Audio(`/sound/success.wav`);
+        audio.play();
+      }, (comColor.length + 1) * 100);
+      setTimeout(() => {
+        comSequence();
+      }, (comColor.length + 1) * 400);
     }
     return;
   }
@@ -88,20 +101,31 @@ function App() {
   }
 
   const playGame = () => {
-    // setButtonIniciar(!buttonInciar);
+    setButtonIniciar(true);
     comSequence();
+  }
+
+  const isDarkMode = () => {
+    setDarkMode(!darkMode);
   }
 
   return (
     <div className="App">
       <div>
         <button onClick={() => playGame()} disabled={buttonInciar}>Iniciar</button>
+        <button onClick={() => isDarkMode()}>Tema de fondo</button>
       </div>
+      <div className="button-container" style={{ backgroundColor: darkMode ? 'black' : 'white', borderRadius: '20%', width: '310px', height: '310px', marginTop: '10px', padding:'10px'}}>
+        <button className='button red' onClick={() => handleClick('red')} style={{backgroundColor: buttonColor === 'red' && isClicked ? '#FF0000' : '#8A3B3B'}} disabled={buttonColorDisable}></button>
+        <button className='button green' onClick={() => handleClick('green')} style={{backgroundColor: buttonColor === 'green' && isClicked ? '#00FF00' : '#5EAB5E'}} disabled={buttonColorDisable}></button>
+        <button className='button yellow' onClick={() => handleClick('yellow')} style={{backgroundColor: buttonColor === 'yellow' && isClicked ? '#FFFF00' : '#B4B43B'}} disabled={buttonColorDisable}></button>
+        <button className='button blue' onClick={() => handleClick('blue')} style={{backgroundColor: buttonColor === 'blue' && isClicked ? '#00BFFF' : '#3A70A5'}} disabled={buttonColorDisable}></button>
+      </div>
+      
       <div>
-        <button className='red' onClick={() => handleClick('red')} style={{backgroundColor: buttonColor === 'red' && isClicked ? 'lightcoral' : 'darkred'}} disabled={buttonColorDisable}>{colors[0]}</button>
-        <button className='green' onClick={() => handleClick('green')} style={{backgroundColor: buttonColor === 'green' && isClicked ? 'lightgreen' : 'darkgreen'}} disabled={buttonColorDisable}>{colors[1]}</button>
-        <button className='yellow' onClick={() => handleClick('yellow')} style={{backgroundColor: buttonColor === 'yellow' && isClicked ? 'lightyellow' : 'darkgoldenrod'}} disabled={buttonColorDisable}>{colors[2]}</button>
-        <button className='blue' onClick={() => handleClick('blue')} style={{backgroundColor: buttonColor === 'blue' && isClicked ? 'lightblue' : 'darkblue'}} disabled={buttonColorDisable}>{colors[3].toString()}</button>
+        <p style={{ width: '300px', display:'flex', justifyContent: 'center', alignItems: 'center', justifyItems: 'center', margin:'auto'}}>
+          Reglamento: para iniciar el juego hay que hacer click en Iniciar. Cada nivel se agrega 1 color a la secuencia, para pasar al siguiente nivel deberas completar la secuencia SIN ERRORES. En caso de equivocarte volveras al principio y deberas apretar en Iniciar para volver a empezar.
+        </p>
       </div>
     </div>
   );
